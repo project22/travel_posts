96f6def00e5c7862f5748d64a420c275601313fa
nate dawg <nglass1988@gmail.com>
2013-07-26 11:14:39 -0700

    added rspec and validation

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ gem 'sqlite3'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 
+gem 'bcrypt-ruby', '3.0.1'
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
-	 has_many :locations
+	validates :name, presence: true, length: { maximum: 50 }
+	validates :email, presence: true
+	validates :password, length: { minimum: 6 }
+
+	has_many :locations
+
+	has_secure_password
 end
