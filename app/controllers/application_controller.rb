8642a008c5c0351df93bf679f29c7a485dc9d41a
JP Berti <jpberti@yahoo.com>
2013-07-29 10:01:06 -0700

    small UI changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def current_user
-  	@current_user = User.find(1) # TODO: change me to the logged in user
-  	# @current_user ||= User.find_by(id: session[:user_id])
-  end
+
 end
