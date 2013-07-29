8ba4b9a08de9b0dbf1778d6edc1ec4c67f430903
JP Berti <jpberti@yahoo.com>
2013-07-29 11:11:36 -0700

    fix migration.  Remove remove_columns.rb

--- db/migrate/20130728004250_remove_columns.rb
+++
@@ -1,9 +0,0 @@
-class RemoveColumns < ActiveRecord::Migration
-  def up
-    remove_column :users, :remember_token
-    remove_column :users, :salt
-    remove_column :users, :fish
-
-
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130729170730) do
+ActiveRecord::Schema.define(version: 20130729013934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,9 +44,11 @@ ActiveRecord::Schema.define(version: 20130729170730) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
+    t.string   "salt"
+    t.string   "fish"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
