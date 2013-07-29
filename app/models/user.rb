cfcf3e191e2188b981f0b03d3a92541279e7dc11
root <root@Natebox.(none)>
2013-07-28 21:24:14 -0700

    some data manipulation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,39 +1,25 @@
 class User < ActiveRecord::Base
-	validates :name, presence: true, length: { maximum: 50 }
-	validates :email, presence: true
-	validates :password, presence: true, length: { minimum: 6 }
-	
-	before_create :set_random_password
- 	before_save :encrypt_password
 
-	has_secure_password
+  before_save { self.email = email.downcase }
+  # before_create :create_remember_token
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence:   true,
+                    format:     { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 
-
-	def authenticate(password)
-    self.fish == BCrypt::Engine.hash_secret(password, self.salt)
-  	end
-	
-	def User.new_remember_token
+  def User.new_remember_token
     SecureRandom.urlsafe_base64
-  	end
-  private
-  
-  # Automatically encrypt the passed in password (which is transient)
-  # setting the user's salt and hash (fish), which are persistent.
-  def encrypt_password
-    unless password.blank?
-      self.salt = BCrypt::Engine.generate_salt
-      self.fish = BCrypt::Engine.hash_secret(password, self.salt)
-    end
-  end
-  
-  # Set a random password for new users (on create) if one is not
-  # passed in (no user has a blank password). They can always reset
-  # it using the password reset feature.
-  def set_random_password
-    if password.blank?
-      password = password_confirmation = BCrypt::Engine.generate_salt
-    end
   end
 
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  # private
+  #    def create_remember_token
+  #      self.remember_token = User.encrypt(User.new_remember_token)
+  #    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130728004250) do
+ActiveRecord::Schema.define(version: 20130729013934) do
 
   create_table "locations", force: true do |t|
     t.float    "latitude"
@@ -38,8 +38,8 @@ ActiveRecord::Schema.define(version: 20130728004250) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
-    t.string   "salt"
-    t.string   "fish"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
