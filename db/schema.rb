4c972f2c92b8c899b53b09feb0e4c1d7f89e1135
nate dawg <nglass1988@gmail.com>
2013-07-29 10:09:25 -0700

    everyting working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130729013934) do
+ActiveRecord::Schema.define(version: 20130729170730) do
 
   create_table "locations", force: true do |t|
     t.float    "latitude"
