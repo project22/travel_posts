7f72111aafcaa965f5b17a1bc376331ceaa124b5
JP Berti <jpberti@yahoo.com>
2013-07-29 18:33:13 -0700

    add functionality to post new comments in Modal and immediately render them in the pane.  Might be a bug though and multiple submits.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,12 +30,13 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
+   
     @post = Post.new(post_params)
 
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @post }
+        format.json { render json: @post, status: :created  }
       else
         format.html { render action: 'new' }
         format.json { render json: @post.errors, status: :unprocessable_entity }
