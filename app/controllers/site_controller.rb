761b940ac3b533f236eb0fd55382acd7a335c26e
JP Berti <jpberti@yahoo.com>
2013-07-31 17:11:43 -0700

    working on add location processs. not working yet.  JSON post is status 400

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,6 +1,6 @@
 class SiteController < ApplicationController
   def index
-  	@user = @current_user
+  	@user = current_user()
   	@location = Location.new(user: @user)
   	@locations = Location.all
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 	has_many :posts
+  has_many :locations
 
   before_save { self.email = email.downcase }
   before_create :create_remember_token
