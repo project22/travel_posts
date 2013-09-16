53d248a6f46b177e9c4cdf6a5be9571fbea42ea5
JP Berti <jpberti@yahoo.com>
2013-09-16 15:10:37 -0700

    fix post actions and clean up text in ui and some styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
      
 
 	   sign_in user
-	   redirect_to user
+	   redirect_to root_url
   else
 
   	 flash.now[:error] = 'Invalid email/password combination'
